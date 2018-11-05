26fd1c6415f4ec3d576bb60aa347282f54147395
root <1501520190@qq.com>
2018-11-05 10:18:40 +0800

    rzz修改了导航栏的链接

---
+++ src/main/java/com/elegantcollection/controller/demo.java
@@ -0,0 +1,12 @@
+package com.elegantcollection.controller;
+
+public class demo {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 0; i < 10; i++) {
+
+        }
+        System.out.println(i);
+    }
+}
